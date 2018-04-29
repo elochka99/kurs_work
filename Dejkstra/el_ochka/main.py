8513eae634acbb13a9cd7354e55ceae06aaec33a
Elena Gorokhova <elochka150899@gmail.com>
2018-04-29 22:48:24 +0300

    Update main.py

--- Dejkstra/el_ochka/main.py
+++ Dejkstra/el_ochka/main.py
@@ -3,8 +3,22 @@ from el_lang.lang import Lang
 import sys
 from el_core.UI import Ui_MainWindow
 
+sys._excepthook = sys.excepthook
+def my_exception_hook(exctype, value, traceback):
+    """
+    method called if program raised and send the data
+    ro the console
+    """
+    print(exctype, value, traceback)
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+sys.excepthook = my_exception_hook
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     widget = Ui_MainWindow()
     widget.show()
-    sys.exit(app.exec_())
+    try:
+        sys.exit(app.exec_())
+    except:
+        pass
